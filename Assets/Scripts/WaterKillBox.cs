376a68b63793034a6b043c031ea0ef75284d00b2
Liam C - Lab 101 <1034370@student.sae.edu.au>
2025-06-11 12:06:19 +1000

    add kill box
    killbox script for water, start if the respawn manager

--- Assets/Scripts/WaterKillBox.cs
+++ Assets/Scripts/WaterKillBox.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WaterKillBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
