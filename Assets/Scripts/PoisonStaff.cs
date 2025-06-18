7673763dbeaf2fc73d952e87877ac9e8c3f12929
Liam C - Lab 101 <1034370@student.sae.edu.au>
2025-06-18 10:10:00 +1000

    update poison staff
    giving poison staff a vfx and some coding logic

--- Assets/Scripts/PoisonStaff.cs
+++ Assets/Scripts/PoisonStaff.cs
@@ -5,8 +5,39 @@ using SeaWizard.Weapons;
 
 public class PoisonStaff : BaseStaff
 {
+    private ParticleSystem poisonVFX;
+    private bool isCasting = false;
+
+    // sets the cast point to my VFX for flame thrower in the scene 
+    protected override void Start()
+    {
+        base.Start();
+        poisonVFX = castPoint.GetComponentInChildren<ParticleSystem>(true);
+        if (poisonVFX != null)
+            poisonVFX.Stop();
+    }
+
+    // function for starting casting by playing the particle effect
+    public override void StartCasting()
+    {
+        if (CanCast() && poisonVFX != null && !isCasting)
+        {
+            poisonVFX.Play();
+            isCasting = true;
+        }
+    }
+    // function to stop playing the casting particle effect 
+    public override void StopCasting()
+    {
+        if (poisonVFX != null && isCasting)
+        {
+            poisonVFX.Stop();
+            isCasting = false;
+        }
+    }
+
     public override void CastSpell()
     {
-        // will update when i work out the spell vfx/effect
+        // fireStaff doesn't use burst casting so not needed.
     }
 }
